f0b0fd848d78698ffe55467a67db7625d4a049bd
NatalieBlack <natalievictoriablack@gmail.com>
2016-05-07 21:40:49 -0400

    add entries

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'entries/new'
+
+  get 'entries/create'
+
+  get 'entries/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
