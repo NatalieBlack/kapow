bf19a303bb4189d593f5089b652ef6e99a6fae23
NatalieBlack <natalievictoriablack@gmail.com>
2016-05-19 19:01:55 -0400

    entry model with pubnub

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,29 @@
 class Entry < ActiveRecord::Base
+
+  validates_presence_of :name
+
+  after_save :pubnub_pub
+
+  def self.pubnub_client
+    @@pubnub ||= Pubnub.new(
+        subscribe_key: ENV['sub-key'], 
+          publish_key: ENV['pub-key'], 
+            connect_callback: lambda {|msg| pubnub.publish(channel: pubnub_channel, message: 'Hello from PubNub Ruby SDK!!', http_sync: true)}
+    )
+  end
+      
+  def self.pubnub_sub
+    pubnub_client.subscribe(channel: pubnub_channel) do |envelope|
+        puts envelope.message
+    end
+  end
+
+  def self.pubnub_channel
+    'entries'
+  end
+
+  def pubnub_pub
+    Entry.pubnub_client.publish(channel: Entry.pubnub_channel, message: name)
+  end
+
 end
