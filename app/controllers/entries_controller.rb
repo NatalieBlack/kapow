d2c0463ec6597783d37c06e17591f01ba8d38278
NatalieBlack <natalievictoriablack@gmail.com>
2016-05-19 19:01:17 -0400

    new entry form

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,10 +1,21 @@
 class EntriesController < ApplicationController
   def new
+    @entry = Entry.new
   end
 
   def create
+    @entry = Entry.new(entry_params)
+    if @entry.save
+      redirect_to new_entry_path, notice: 'Thank you!'
+    else
+      render :new
+    end
   end
 
   def index
+  end
+
+  def entry_params
+    params.require(:entry).permit(:name)
   end
 end
